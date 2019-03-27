574d172b2af722ba3fdbde2b56298d70cc99f259
Alexander Dolnik <adolnik@marlen-dress.ru>
2019-03-27 15:44:59 +0300

    =Run unit tests from main class

--- src/main/java/com/avvo/avvohivetest/TMain.java
+++ src/main/java/com/avvo/avvohivetest/TMain.java
@@ -1,9 +1,19 @@
 package com.avvo.avvohivetest;
 
 import java.io.*;
+import org.junit.*;
+import org.junit.runner.*;
 
 public class TMain
 {
+    public static void resultReport(Result result) {
+        System.out.println("Finished. Result: Failures: " +
+                result.getFailureCount() + ". Ignored: " +
+                result.getIgnoreCount() + ". Tests run: " +
+                result.getRunCount() + ". Time: " +
+                result.getRunTime() + "ms.");
+    }
+
     public static void main(String [] args)
     {
         System.out.println("Test project");
@@ -13,6 +23,12 @@ public class TMain
         }catch(Exception e){
             System.out.println(e.toString());
         }
+
+        //org.junit.runner.JUnitCore.main("com.avvo.avvohivetest.HelloHiveRunnerTest");
+        JUnitCore junit = new JUnitCore();
+        //junit.addListener(new TextListener(System.out));
+        Result result = junit.run(HelloHiveRunnerTest.class);
+        resultReport(result);
     }
 
 }
