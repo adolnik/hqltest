d138a80eb0401fbaebdbaf5d411f57fc3998d253
Alexander Dolnik <adolnik@marlen-dress.ru>
2019-03-27 10:18:00 +0300

    first commit

---
+++ src/main/java/com/avvo/avvohivetest/TMain.java
@@ -0,0 +1,12 @@
+package com.avvo.avvohivetest;
+
+import java.io.*;
+
+public class TMain
+{
+    public static void main(String [] args)
+    {
+        System.out.println("Test project");
+    }
+
+}
