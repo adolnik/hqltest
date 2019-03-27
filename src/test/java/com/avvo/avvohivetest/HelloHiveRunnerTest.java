23a0516bc030fe6962e96ea32e11cbd72c8e208e
Alexander Dolnik <adolnik@marlen-dress.ru>
2019-03-27 14:48:12 +0300

    =Fixed test error

--- src/test/java/com/avvo/avvohivetest/HelloHiveRunnerTest.java
+++ src/test/java/com/avvo/avvohivetest/HelloHiveRunnerTest.java
@@ -38,7 +38,7 @@ import static org.junit.Assert.assertEquals;
  * All HiveRunner tests should run with the StandaloneHiveRunner and have a reference to HiveShell.
  */
 @RunWith(StandaloneHiveRunner.class)
-public class HelloHiveRunner {
+public class HelloHiveRunnerTest {
     @HiveSQL(files = {})
     private HiveShell shell;
 
